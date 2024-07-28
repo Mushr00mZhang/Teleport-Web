7b723a1665b7a6192c4649fda3445c3692c261c5
Mushr00mZhang <392388987@qq.com>
2024-07-28 11:27:17 +0800

    新增登录名校验

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -3,7 +3,12 @@
     <header class="teleport-login-header"></header>
     <main class="teleport-login-main">
       <form @submit.prevent="login">
-        <md-outlined-text-field label="登录名" v-model="client.loginName"></md-outlined-text-field>
+        <md-outlined-text-field
+          label="登录名"
+          v-model="client.loginName"
+          :error="loginNameCheck.error"
+          :error-text="loginNameCheck.text"
+        ></md-outlined-text-field>
         <md-outlined-text-field label="昵称" v-model="client.nickName"></md-outlined-text-field>
         <md-filled-button type="submit">登录</md-filled-button>
       </form>
@@ -14,7 +19,7 @@
 <script setup lang="ts">
 import '@material/web/textfield/outlined-text-field';
 import '@material/web/button/filled-button';
-import { reactive } from 'vue';
+import { computed, reactive } from 'vue';
 import { useChatStore } from '@/store/chat';
 import { useRouter } from 'vue-router';
 const router = useRouter();
@@ -23,6 +28,26 @@ const client = reactive({
   url: 'api/login',
   loginName: '',
   nickName: '',
+});
+const loginNameCheck = computed(() => {
+  if (client.loginName) {
+    if (!/^[A-Za-z0-9]+$/.test(client.loginName)) {
+      return {
+        error: true,
+        text: '请输入英文或数字',
+      };
+    }
+    if (client.loginName.length > 20) {
+      return {
+        error: true,
+        text: '最大长度不得超过20',
+      };
+    }
+  }
+  return {
+    error: false,
+    text: '',
+  };
 });
 const login = async () => {
   chatStore.login(client.loginName, client.nickName);

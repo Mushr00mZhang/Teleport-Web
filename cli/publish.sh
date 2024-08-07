6683b5d177a95434fc6b3e16825597e28999e8dc
比香菇傻一点 <392388987@qq.com>
2024-08-07 16:07:56 +0800

    更新部署文件

--- cli/publish.sh
+++ cli/publish.sh
@@ -1,3 +1,3 @@
 docker load <teleport_web.tar
-docker compose down
-docker compose up -d --remove-orphans
+docker compose -p telteport_web down
+docker compose -p telteport_web up -d --remove-orphans
